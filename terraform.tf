ff2620cf119c7cbb8b3e06cc264cba2cb02376c1
Ray Kao <ray@peopleandcode.com>
2018-01-25 23:43:51 -0500

    commented out tf.backend config - can't use interpolation for backend config

--- terraform.tf
+++ terraform.tf
@@ -1,17 +1,30 @@
-terraform {
-  backend "azurerm" {
-    storage_account_name = "${var.backend_storage_account_name}"
-    container_name       = "${var.backend_container_name}"
-    key                  = "${var.backend_key}"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     storage_account_name = "${var.backend_storage_account_name}"
+#     container_name       = "${var.backend_container_name}"
+#     key                  = "${var.backend_key}"
+#   }
+# }
+## THIS WILL NOT WORK - terraform.backend: configuration cannot contain interpolations
+#
+# The backend configuration is loaded by Terraform extremely early, before
+# the core of Terraform can be initialized. This is necessary because the backend
+# dictates the behavior of that core. The core is what handles interpolation
+# processing. Because of this, interpolations cannot be used in backend
+# configuration.
+#
+# If you'd like to parameterize backend configuration, we recommend using
+# partial configuration with the "-backend-config" flag to "terraform init".
+#
+# see: https://www.terraform.io/docs/backends/config.html#partial-configuration 
+#
 
-resource "azurerm_resource_group" "vmss" {
+resource "azurerm_resource_group" "test" {
   name     = "${var.resource_group_name}"
   location = "${var.location}"
 
   tags {
-    environment = "codelab"
+    environment = "terraformtest"
   }
 }
 
