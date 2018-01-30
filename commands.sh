716eda0c1a873b0feaef0abab1b692c24fe4f736
Ray Kao <ray@peopleandcode.com>
2018-01-30 17:37:21 -0500

    testing how environments are handled in vsts

--- commands.sh
+++ commands.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 
-az login --service-principal -u $AZ_SERVICE_PRINCIPAL_ID -p $AZ_SERVICE_PRINCIPAL_SECRET --tenant $AZ_TENANT_ID
+# az login --service-principal -u $AZ_SERVICE_PRINCIPAL_ID -p $AZ_SERVICE_PRINCIPAL_SECRET --tenant $AZ_TENANT_ID
 
-az account set --subscription="$AZ_SUBSCRIPTION_ID"
+# az account set --subscription="$AZ_SUBSCRIPTION_ID"
 
-terraform init \
-    -backend-config="access_key=$TF_VAR_backend_access_key" \
-    -backend-config="storage_account_name=$TF_VAR_backend_storage_account_name" \
-    -backend-config="container_name=$TF_VAR_backend_container_name" \
-    -backend-config="key=$TF_VAR_backend_key"
+# terraform init \
+#     -backend-config="access_key=$TF_VAR_backend_access_key" \
+#     -backend-config="storage_account_name=$TF_VAR_backend_storage_account_name" \
+#     -backend-config="container_name=$TF_VAR_backend_container_name" \
+#     -backend-config="key=$TF_VAR_backend_key"
 
-terraform apply
+# terraform apply
+
+env
