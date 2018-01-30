8f8ea0a83f17c90b119211e3a835edfebafe7cd7
Ray Kao <ray@peopleandcode.com>
2018-01-30 18:23:44 -0500

    fixed variables name to include prefixes...

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 # terraform backed settings: https://www.terraform.io/docs/backends/config.html
 # we're using azurerm back in terraform.tf
 
-variable "SUBSCRIPTION_ID" {}
-variable "SERVICE_PRINCIPAL_ID" {}
-variable "SERVICE_PRINCIPAL_SECRET" {}
-variable "TENANT_ID" {}
+variable "AZ_SUBSCRIPTION_ID" {}
+variable "AZ_SERVICE_PRINCIPAL_ID" {}
+variable "AZ_SERVICE_PRINCIPAL_SECRET" {}
+variable "AZ_TENANT_ID" {}
 variable "resource_group_name" {}
 variable "location" {}
