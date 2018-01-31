9e5cabe5edc66412eb5f842273b1e51bd0a0668f
Ray Kao <ray@peopleandcode.com>
2018-01-30 19:39:42 -0500

    added auto approve to tf apply

--- commands.sh
+++ commands.sh
@@ -10,4 +10,4 @@ echo $TF_VAR_BACKEND_ACCESS_KEY
 
 terraform init -backend-config="access_key=$TF_VAR_BACKEND_ACCESS_KEY" -backend-config="storage_account_name=$TF_VAR_BACKEND_STORAGE_ACCOUNT_NAME" -backend-config="container_name=$TF_VAR_BACKEND_CONTAINER_NAME" -backend-config="key=$TF_VAR_BACKEND_KEY"
 
-terraform apply
+terraform apply --auto-approve
